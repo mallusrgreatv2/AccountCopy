62db70a874346e11a6c0c9dae7c44b37b894b708
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2024-01-03 17:54:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// | CONFIGURATION
-// | - When the file starts,
-// | - Replace the "(your token)" with your actual token.
-// | - To put user IDs create a new line and add text like this: "USER_ID_1=(user id)". Change the user ID.
-// | - You can create any number of user IDs. Any users with these IDs can use the commands provided by this selfbot
-
-// | DOCUMENTATION
-// | - How to use
-// |   - Impersonation
-// |     In the host account use the command >>impersonate followed by the user or user ID
-// |   - Copy Pasting
-// |     Configure IDs above.
-// |     Make a server with all the accounts.
-// |     In the host account use command >>copy in the server.
-// |     In other accounts use >>paste in the same server.
-
 // const { Client } = require("discord.js-selfbot-v13");
 const fs = require("fs");
 let Client;
